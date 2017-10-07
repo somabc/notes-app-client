e91ebf53dd82b66b2def376918cf3bf2642dd8d9
bryan cochrane <bcochran@bryans-air.home>
2017-10-07 17:20:17 +0100

    cognito

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
+import "../config";
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUser
+} from "amazon-cognito-identity-js";
 
 export default class Login extends Component {
   constructor(props) {
@@ -12,6 +18,23 @@ export default class Login extends Component {
     };
   }
 
+  login(email, password) {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID
+    });
+    const user = new CognitoUser({ Username: email, Pool: userPool });
+    const authenticationData = { Username: email, Password: password };
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    return new Promise((resolve, reject) =>
+      user.authenticateUser(authenticationDetails, {
+        onSuccess: result => resolve(),
+        onFailure: err => reject(err)
+        })
+      );
+    }
+
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
@@ -22,9 +45,16 @@ export default class Login extends Component {
     });
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
+  handleSubmit = async event => {
+  event.preventDefault();
+
+  try {
+    await this.login(this.state.email, this.state.password);
+    alert("Logged in");
+  } catch (e) {
+    alert(e);
   }
+}
 
   render() {
     return (
