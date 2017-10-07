85bbeb3dae82625f811f5882abe51a74cde7cd0e
bryan cochrane <bcochran@bryans-air.home>
2017-10-07 17:47:37 +0100

    load state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Nav, NavItem, Navbar } from "react-bootstrap";
-import "./App.css";
 import Routes from "./Routes";
+import { authUser } from "./libs/awsLib";
 import RouteNavItem from "./components/RouteNavItem";
-
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-  super(props);
+    super(props);
 
-  this.state = {
-    isAuthenticated: false
-  };
-}
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
 
-userHasAuthenticated = authenticated => {
-  this.setState({ isAuthenticated: authenticated });
-}
+  async componentDidMount() {
+    try {
+      if (await authUser()) {
+        this.userHasAuthenticated(true);
+      }
+    }
+    catch(e) {
+      alert(e);
+    }
 
-handleLogout = event => {
-  this.userHasAuthenticated(false);
-}
+    this.setState({ isAuthenticating: false });
+  }
 
-render() {
-  const childProps = {
-    isAuthenticated: this.state.isAuthenticated,
-    userHasAuthenticated: this.userHasAuthenticated
-  };
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  }
+
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
@@ -39,16 +55,16 @@ render() {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-            {this.state.isAuthenticated
-              ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
-              : [
-                  <RouteNavItem key={1} href="/signup">
-                    Signup
-                  </RouteNavItem>,
-                  <RouteNavItem key={2} href="/login">
-                    Login
-                  </RouteNavItem>
-                ]}
+              {this.state.isAuthenticated
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : [
+                    <RouteNavItem key={1} href="/signup">
+                      Signup
+                    </RouteNavItem>,
+                    <RouteNavItem key={2} href="/login">
+                      Login
+                    </RouteNavItem>
+                  ]}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
