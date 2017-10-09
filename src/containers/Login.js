5cb91e91d2b777214f101ccc19646af04c8bb158
bryan cochrane <bcochran@bryans-air.home>
2017-10-09 17:07:06 +0100

    router

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 import "../config";
 import {
@@ -46,7 +47,19 @@ export default class Login extends Component {
   }
 
   handleSubmit = async event => {
-  event.preventDefault();
+    event.preventDefault();
+
+    this.setState({ isLoading: true });
+
+    try {
+      await this.login(this.state.email, this.state.password);
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
 
   try {
     await this.login(this.state.email, this.state.password);
@@ -77,12 +90,15 @@ export default class Login extends Component {
               type="password"
             />
           </FormGroup>
-          <Button
+          <LoaderButton
             block
             bsSize="large"
             disabled={!this.validateForm()}
             type="submit"
-          >
+            isLoading={this.state.isLoading}
+            text="Login"
+            loadingText="Logging in…"
+          />
             Login
           </Button>
         </form>
