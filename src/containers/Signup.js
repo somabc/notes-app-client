0005bcef4852970068262d6f9dc429be51355a3d
bryan cochrane <bcochran@bryans-air.home>
2017-10-09 17:16:00 +0100

    aws cognito

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -41,14 +41,87 @@ export default class Signup extends Component {
   }
 
   handleSubmit = async event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    this.setState({ isLoading: true });
+  this.setState({ isLoading: true });
 
-    this.setState({ newUser: "test" });
+  try {
+    const newUser = await this.signup(this.state.email, this.state.password);
+    this.setState({
+      newUser: newUser
+    });
+  } catch (e) {
+    alert(e);
+  }
 
+  this.setState({ isLoading: false });
+}
+
+handleConfirmationSubmit = async event => {
+  event.preventDefault();
+
+  this.setState({ isLoading: true });
+
+  try {
+    await this.confirm(this.state.newUser, this.state.confirmationCode);
+    await this.authenticate(
+      this.state.newUser,
+      this.state.email,
+      this.state.password
+    );
+
+    this.props.userHasAuthenticated(true);
+    this.props.history.push("/");
+  } catch (e) {
+    alert(e);
     this.setState({ isLoading: false });
   }
+}
+
+signup(email, password) {
+  const userPool = new CognitoUserPool({
+    UserPoolId: config.cognito.USER_POOL_ID,
+    ClientId: config.cognito.APP_CLIENT_ID
+  });
+
+  return new Promise((resolve, reject) =>
+    userPool.signUp(email, password, [], null, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(result.user);
+    })
+  );
+}
+
+confirm(user, confirmationCode) {
+  return new Promise((resolve, reject) =>
+    user.confirmRegistration(confirmationCode, true, function(err, result) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
+    })
+  );
+}
+
+authenticate(user, email, password) {
+  const authenticationData = {
+    Username: email,
+    Password: password
+  };
+  const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+  return new Promise((resolve, reject) =>
+    user.authenticateUser(authenticationDetails, {
+      onSuccess: result => resolve(),
+      onFailure: err => reject(err)
+    })
+  );
+}
 
   handleConfirmationSubmit = async event => {
     event.preventDefault();
