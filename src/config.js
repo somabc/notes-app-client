b68c53d767d94f704b0c1c73d8bb1ea6db472740
bcochran <b.cochrane@imperial.ac.uk>
2017-10-10 17:39:47 +0100

    notes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID"
+    USER_POOL_ID: "eu-west-2_hwTpXir6K",
+    APP_CLIENT_ID: "39ccsoeve71591e9n63k340jq0"
   }
 };
