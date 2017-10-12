e7a1fb3c21f38cbccbfd57bec15382fead3cc219
bcochran <somabc@gmail.com>
2017-10-12 16:42:34 +0100

    config

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
 },
   cognito: {
     USER_POOL_ID: "eu-west-2_hwTpXir6K",
-    APP_CLIENT_ID: "39ccsoeve71591e9n63k340jq0"
+    APP_CLIENT_ID: "39ccsoeve71591e9n63k340jq0",
     REGION: "eu-west-2",
     IDENTITY_POOL_ID: "eu-west-2:d38f353f-c2c2-4ee8-a5b0-6b83e4613894",
   }
